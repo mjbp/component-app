1e2d1168b303df26d4c8897c6334ae70457af1a6
Mick Perkins <mickperkins@gmail.com>
2017-02-22 23:05:20 +0000

    Updates service worker path for gh-pages

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ const init = () => {
     store.subscribe(renderList);
     API(store, actions);
 
-    if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
+    if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('sw.js'));
 };
 
 const renderList = () => {
