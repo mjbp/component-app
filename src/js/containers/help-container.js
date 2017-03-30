de26da83e2bdde7e6b11482fcbcbddc6446eadea
mjbp <mick.perkins@stormid.com>
2017-03-30 17:44:31 +0100

    Adds webpack-dev-server, starts help

---
+++ src/js/containers/help-container.js
@@ -0,0 +1,6 @@
+import { HelpIcon } from '../components/help-icon';
+// import { HelpContent } from '../components/help-content';
+
+import styles from '../../css/help.css'
+
+export const HeaderContainer = (store, actions) => HelpIcon;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const webpack = require('webpack');
+// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require('path');
+const HOST = 'localhost';
+const PORT = 8080;
+const PROXY = `http://${HOST}:${PORT}`;
+const ASSET_PATH = path.resolve('src/')
+const APP_FILE_PATH = `${ASSET_PATH}/js/index.js`
 
 module.exports = {
-    entry: "./src/js/index.js",
+    entry: APP_FILE_PATH,
     output: {
-        path: './build/',
+        path: __dirname + '/build/',
         filename: "app.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        host: HOST,
+        port: PORT,
+        hot: true,
+        inline: true
     },
     module: {
          loaders: [
              {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: ['babel-loader']
+                loader: ['babel-loader'],
+                include: [
+                    `${ASSET_PATH}/js`, // Cuts build time in half
+                ],
             },
             {
                 test: /\.css/,
@@ -37,6 +56,7 @@ module.exports = {
                 minifyURLs: true
             }
         }),
-        new HtmlWebpackInlineSourcePlugin()
+        new HtmlWebpackInlineSourcePlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
